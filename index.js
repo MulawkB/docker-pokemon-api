82c76a56d3dff55e337c0f0c2eeed33cacb2ba48
Brian Mulawka <mulawkabrian@gmail.com>
2026-05-22 10:50:10 +0200

    first commit

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ createFirstUser = require('./src/db/create-first-user');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/pokemon-api-rest-Brian-Mulawka')
-    .then(() => console.log('connexion à MongoDB réussie !'))
-    .catch(err => console.error(err));
-
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-createFirstUser.createFirstUser();
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/pokemon-api-rest-Brian-Mulawka')
+    .then(() => { 
+        console.log('connexion à MongoDB réussie !')
 
-app.post('/api/login', userRoutes.userLogin);
+        createFirstUser.createFirstUser();
 
-app.use(authMdlr);
+        app.post('/api/login', userRoutes.userLogin);
 
-app.get('/api/pokemons', pokemonRoutes.findAllPokemons);
-app.get('/api/pokemons/:id', pokemonRoutes.findPokemonByPk);
-app.post('/api/pokemons', pokemonRoutes.createPokemon);
-app.put('/api/pokemons/:id', pokemonRoutes.updatePokemon);
-app.delete('/api/pokemons/:id', pokemonRoutes.deletePokemon);
+        app.use(authMdlr);
 
-app.use((req, res) => res.json({ message: 'not found' }));
-app.listen(8000, () => { console.log('app listening on port 8000') });
+        app.get('/api/pokemons', pokemonRoutes.findAllPokemons);
+        app.get('/api/pokemons/:id', pokemonRoutes.findPokemonByPk);
+        app.post('/api/pokemons', pokemonRoutes.createPokemon);
+        app.put('/api/pokemons/:id', pokemonRoutes.updatePokemon);
+        app.delete('/api/pokemons/:id', pokemonRoutes.deletePokemon);
+
+        app.use((req, res) => res.json({ message: 'not found' }));
+        app.listen(8000, () => { console.log('app listening on port 8000') });
+    })
+    .catch(err => console.error(err));
